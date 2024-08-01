4d2a13ae15e587eae11f6b0bff400ee6164b6ad0
BarronWill <viet22082003@gmail.com>
2024-08-01 23:45:36 +0700

    Optimize Naive Bayes Code

--- Naive-Bayes/util.py
+++ Naive-Bayes/util.py
@@ -3,7 +3,13 @@ import pandas as pd
 
 class NaiveBayes:
   def __init__(self):
-    pass
+    self.df = None
+    self.prob = {}
+    self.classes = None
+    self.px = 0
+    self.pxci = None
+    self.result = {}
+
 
   def find_laplace_prob(self, df, c, labels):
     # print("{} - {} ".format(df[df == c].shape[0], df.shape[0]))
@@ -13,26 +19,19 @@ class NaiveBayes:
   def fit(self , df):
     self.df = df
     self.classes = df.iloc[:,-1].unique()
-    self.prob = {}
     for c in self.classes:
       self.prob[c] = self.find_laplace_prob(df.iloc[:,-1], c, len(self.classes))
 
 
   def predict(self, x):
-    self.px = 0
     self.pxci = {c : 1 for c in self.classes}
-    self.result = {}
-    df = self.df
-    columns = df.columns
+    columns = self.df.columns
     for c in self.classes:
-      index = 0
-      for features in x:
-        num_labels = df[columns[index]].nunique()
-        filtered_df = df[df[columns[-1]] == c][columns[index]]
-        self.pxci[c] = self.pxci[c] * self.find_laplace_prob(filtered_df, features, num_labels)
+      for index, feature in enumerate(x):
+        num_labels = self.df[columns[index]].nunique()
+        filtered_df = self.df[self.df[columns[-1]] == c][columns[index]]
+        self.pxci[c] = self.pxci[c] * self.find_laplace_prob(filtered_df, feature, num_labels)
         # print("{} - {} - {} - {}".format(features, c, num_labels, self.find_laplace_prob(filtered_df, features, num_labels)))
-        index = index + 1
       self.px += self.pxci[c] * self.prob[c]
-
     for c in self.classes:
       self.result[c] = (self.pxci[c] * self.prob[c]) / self.px
